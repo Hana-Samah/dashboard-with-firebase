b9d0a7fcbb411f48037c930077e4e9308f638114
Hana Samah <hano.9009o@gmail.com>
2025-05-02 11:00:29 +0300

    sec commit

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.1/firebas
 
 // إعدادات Firebase الخاصة بك
 const firebaseConfig = {
-  apiKey: "API_KEY_HERE",
-  authDomain: "AUTH_DOMAIN_HERE",
-  projectId: "PROJECT_ID_HERE",
-  storageBucket: "STORAGE_BUCKET_HERE",
-  messagingSenderId: "SENDER_ID_HERE",
-  appId: "APP_ID_HERE"
-};
+    apiKey: "AIzaSyBBNa4CDG7H6LwX_UeMdB9EL5bAB0z4OHE",
+    authDomain: "login-form-bbdb1.firebaseapp.com",
+    projectId: "login-form-bbdb1",
+    storageBucket: "login-form-bbdb1.appspot.com", // أصلحي spelling من `.app` إلى `.appspot.com`
+    messagingSenderId: "287126610512",
+    appId: "1:287126610512:web:ddcecdf06ed7d11eabe755",
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
